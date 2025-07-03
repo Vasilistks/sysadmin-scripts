fd235ce6de0cf021066ef344f086184436a929ce
vtks <billtks@homeserver14.xyz>
2025-07-03 23:29:09 +0300

    Add initial hello_sysadmin.sh script

---
+++ hello_sysadmin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# A simple script to greet the sysadmin
+echo "Hello, aspiring Linux SysAdmin! Welcome to your scripts repository."
+echo "Current date and time: $(date)"
+echo "Your hostname: $(hostname)"
